1bb5ccafc05494307b4c92daa23013b49be54522
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-26 23:46:27 +0300

    Update views.py - gallery app

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gallery(request):
+    return render(request, gallery.html)
+
+def viewPhoto(request,pk):
+    return render(request, viewPhoto.html)
+
+def addPhoto(request):
+    return render(request, addPhoto.html)
