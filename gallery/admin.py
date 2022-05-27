c745da45d2dbdc5b702344d68a62ac251de3773f
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-28 01:00:07 +0300

    Correct indentation error in models

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import Category,Pictures,Location
 
-admin.site.Register(Category),
-admin.site.Register(Image)
-admin.site.Register(Location)
+admin.site.register(Category),
+admin.site.register(Pictures),
+admin.site.register(Location),
