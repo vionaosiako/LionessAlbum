43470545ce929968825dfe306e3cd210637fd10e
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-28 00:11:59 +0300

    Add Category class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from cloudinary.models import CloudinaryField
+import datetime as dt
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
+
+    @classmethod
+    def display_all_categories(cls):
+        return cls.objects.all()
