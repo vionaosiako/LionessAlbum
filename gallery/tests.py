57e2fe2594b85f33e3d853b917c77128252ab9a4
vionaosiako <viona.osiako@student.morigaschool.com>
2022-05-30 08:41:53 +0300

    Update test.py

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Pictures,Category,Location
+
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.rare = Category(name="Boooks")
+
+    def test_save_category(self):
+        self.rare.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)
+
+
